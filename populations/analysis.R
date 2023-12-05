71ee1213613c2b3c143a77062b23645946a34f0f
Jose V. Die <jodiera@upv.es>
2023-12-05 15:54:01 +0100

    Create analysis.R

---
+++ populations/analysis.R
@@ -0,0 +1,44 @@
+# Dependencies
+library(dplyr)
+library(stringr)
+library(tidyr)
+
+# load function for analysis
+source("functions2.R")
+
+# load dataset
+load("dat/rip1.rda")
+
+
+# Phenotipic Data 
+# -----------------------------------------------------------------------------
+
+# Case study 1 : mean of average of repetitions ------------------------------------------
+rip %>% 
+  filter(RIL %in% c(52, 67)) %>% 
+  group_by(RIL, repetition) %>% 
+  summarize("dias" = mean(dias)) %>% 
+  ungroup() %>% 
+  group_by(RIL) %>% 
+  summarize(across("dias", list(mean = mean, sd = sd)))
+
+rip %>% 
+  group_by(RIL, repetition) %>% 
+  summarize("dias" = mean(dias)) %>% 
+  ungroup() %>% 
+  group_by(RIL) %>% 
+  summarize(across("dias", list(mean = mean, sd = sd)))
+  arrange(desc(dias_mean))
+
+# Case study 2 : mean of 1st day to flower per repition  -----------------------
+rip %>% 
+  filter(RIL %in% c(52, 67)) %>% 
+  group_by(RIL, repetition) %>% 
+  summarise(dias = min(dias)) %>% 
+  ungroup() %>% 
+  group_by(RIL) %>% 
+  summarize(across("dias", list(mean = mean, sd = sd))) %>% 
+  arrange(desc(dias_mean))
+    
+
+
